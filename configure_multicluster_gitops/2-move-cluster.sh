652a00c0936d9fef4e4bbf4cbdec2de9086c62d8
prossi-rh <prossi@redhat.com>
2022-10-15 14:35:10 +0200

    Configure ACM / GitOps Integration

---
+++ configure_multicluster_gitops/2-move-cluster.sh
@@ -0,0 +1,2 @@
+oc label managedcluster ocp4stage "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite
+oc label managedcluster local-cluster "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite
