86af96fdbf82c6b0ba7f8a810e73cce34e12b30c
prossi-rh <prossi@redhat.com>
2023-02-09 11:05:31 +0100

    new demo work

--- configure_multicluster_gitops/2-move-cluster.sh
+++ configure_multicluster_gitops/2-move-cluster.sh
@@ -1,3 +1,4 @@
 oc label managedcluster ocp4stage "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite -n open-cluster-management
 oc label managedcluster local-cluster "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite -n open-cluster-management
 oc label managedcluster ocp4dev "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite -n open-cluster-management
+
---
+++ piotrminkowski_gitops/3bis-label.sh
@@ -0,0 +1,2 @@
+oc label managedcluster ocp4dev "cluster.open-cluster-management.io/clusterset=demo" --overwrite -n open-cluster-management
+oc label managedcluster local-cluster "cluster.open-cluster-management.io/clusterset=
