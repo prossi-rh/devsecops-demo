c253700a29c8dcb8b20a6d37cb9fb8fde04c53cb
prossi-rh <prossi@redhat.com>
2023-02-20 16:49:22 +0100

    modified multicluster gitops

--- configure_multicluster_gitops/2-move-cluster.sh
+++
@@ -1,4 +0,0 @@
-oc label managedcluster ocp4stage "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite -n open-cluster-management
-oc label managedcluster local-cluster "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite -n open-cluster-management
-oc label managedcluster ocp4dev "cluster.open-cluster-management.io/clusterset=all-clusters" --overwrite -n open-cluster-management
-
