8e9876fff73bdddc77db800856bee90dedc2c7d5
prossi-rh <prossi@redhat.com>
2023-02-20 16:50:43 +0100

    Deleted unuseful yaml for gitops

--- piotrminkowski_gitops/3bis-label.sh
+++
@@ -1,2 +0,0 @@
-oc label managedcluster ocp4dev "cluster.open-cluster-management.io/clusterset=demo" --overwrite -n open-cluster-management
-oc label managedcluster local-cluster "cluster.open-cluster-management.io/clusterset=
